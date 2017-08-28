d3514789ef7f50ec724e9c3f236af3bb205c6f80
Brack Quillen <quillen.brack@bitsourceky.com>
2017-08-28 14:23:05 -0400

    changing files

---
+++ business card/Assets/EmptyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+public class PlayClass: MonoBehaviour {
+	public MovieTexture movTexture; 
+	void Start(){
+		GetComponent<Renderer> ().material.mainTexture = movTexture;
+		movTexture.Play ();
+		movTexture.loop = true;
+	}
+
+}
+
---
+++ business card/Assets/VBScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VBScript : MonoBehaviour, IVirtualButtonEventHandler {
+	private	GameObject VBButtonObject;
+	private GameObject zombie; 
+	// Use this for initialization
+	void Start () {
+		VBButtonObject = GameObject.Find ("actionButton");
+		zombie = GameObject.Find ("zombie");
+		VBButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb) {
+		Debug.Log ("button pressed");
+		zombie.GetComponent<Animation> ().Play ();
+
+	}
+
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
+		zombie.GetComponent<Animation> ().Stop ();
+	}
+}
+
+
